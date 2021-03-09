eb0b18bc5345e6edb59708396642dfd900c74e69
Andrey Bandura <andretan322@gmail.com>
2021-03-09 17:30:21 +0200

    add some features

---
+++ index.js
@@ -0,0 +1,13 @@
+function cookiesHidden() {
+    document.getElementById("cookies").style.display = "none";
+}
+
+function background() {
+    document.getElementById("background").style.display = "block";
+    document.getElementById("hire").style.display = "block";
+}
+
+function backgroundNone() {
+    document.getElementById("background").style.display = "none";
+    document.getElementById("hire").style.display = "none";
+}
